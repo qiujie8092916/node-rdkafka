71bb6434d0ff62c7d43072a68ae1510211a6dcc5
jie.q <qiujieee_empty@outlook.com>
2024-11-08 15:00:36 +0800

    feature: run test refs #275665

--- test/consumer.spec.js
+++ test/consumer.spec.js
@@ -7,7 +7,7 @@
  * of the MIT license.  See the LICENSE.txt file for details.
  */
 
-var addon = require('bindings')('node-librdkafka');
+var addon = require('bindings')('node-rdkafka');
 var t = require('assert');
 
 var client;
